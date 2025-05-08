d9888d777d3a2696b13a2620baca56337969df4e
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-05-08 18:12:31 +0600

    solved all problems

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function formatString(input, toUpper = true) {
-    return toUpper ? input.toUpperCase() : input.toLowerCase();
-}
-console.log(formatString("Hello"));
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-function formatString(input: string, toUpper: boolean = true): string {
-  return toUpper ? input.toUpperCase() : input.toLowerCase();
-}
-
-console.log(formatString("Hello", false));
